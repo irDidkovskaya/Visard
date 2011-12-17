2eb865ead5756c7d2c751234f4bd9efa391a6678
Irina <irdidkovskaya@gmail.com>
2011-12-17 12:01:20 +0200

    Initial Commit

---
+++ vc-mobile/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  vc-mobile
+//
+//  Created by Ирина Дидковская on 17.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ vc-mobile/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  vc-mobile
+//
+//  Created by Ирина Дидковская on 17.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
